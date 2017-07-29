9dbdc06574ff422087dc864f368db58a078f1f1f
Kl000 <camillestluigi@gmail.com>
2017-07-29 17:40:37 +0200

    add of the 3 differents pipes

---
+++ Assets/canCreateSmoke.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class canCreateSmoke : MonoBehaviour {
+
+	public float cooldown;
+	private float _nextSmoke;
+	public ParticleSystem particleSys;
+
+	// Use this for initialization
+	void Start () {
+		
+		this.cooldown = 4;
+		this._nextSmoke = Time.realtimeSinceStartup + cooldown;
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+		if (Time.realtimeSinceStartup > _nextSmoke) {
+
+			this.particleSys.Play ();
+			this._nextSmoke = Time.realtimeSinceStartup + this.cooldown;
+
+		}
+
+	}
+
+}
