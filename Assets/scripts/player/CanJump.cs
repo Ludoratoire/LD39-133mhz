5238ef2b2684ccdc5e74d66b42d36f8e0996383c
OuichesDoctor <junkerlucas@gmail.com>
2017-07-29 11:27:30 +0200

    Add player base (move + jump)

---
+++ Assets/scripts/player/CanJump.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanJump : MonoBehaviour {
+
+    // Inspector fields
+
+    public float inputThreshold = 0.1f;
+    public float jumpForce = 10f;
+
+    // Components
+
+    protected Rigidbody2D _rb2d;
+
+    // Private fields
+
+    protected bool _requestJump = false;
+    protected bool _touchingGround = false;
+
+	void Start () {
+
+        _rb2d = GetComponent<Rigidbody2D>();
+
+	}
+	
+	void Update () {
+
+        var yAxis = Input.GetAxis("Vertical");
+        if (_touchingGround && yAxis > inputThreshold)
+            _requestJump = true;
+
+	}
+
+    void FixedUpdate() {
+
+        if(_requestJump) {
+            _rb2d.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            _requestJump = false;
+            _touchingGround = false;
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision) {
+
+        if(collision.gameObject.layer == LayerMask.NameToLayer("Ground")) {
+            _touchingGround = true;
+        }
+
+    }
+}
---
+++ Assets/scripts/player/CanWalk.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CanWalk : MonoBehaviour {
+
+    public enum MoveDirection : int {
+        Left = -1,
+        Right = 1,
+        None = 0
+    }
+
+    // Inspector fields
+
+    public float unitsPerSecond = 1f;
+    public float inputThreshold = 0.1f;
+
+    // Components
+
+    protected Rigidbody2D _rb2d;
+    protected SpriteRenderer _spriteRenderer;
+
+    // Private fields
+
+    protected MoveDirection _direction;
+    protected bool _facingRight = true;
+
+    // Methods
+
+	void Start () {
+
+        _rb2d = GetComponent<Rigidbody2D>();
+        _spriteRenderer = GetComponent<SpriteRenderer>();
+
+	}
+	
+	void Update () {
+
+        // Read inputs
+
+        var xAxis = Input.GetAxis("Horizontal");
+        if (xAxis > inputThreshold) {
+            _direction = MoveDirection.Right;
+            if(!_facingRight) {
+                _facingRight = true;
+                _spriteRenderer.flipX = false;
+            }
+        }
+        else if (xAxis < -inputThreshold) {
+            _direction = MoveDirection.Left;
+            if (_facingRight) {
+                _facingRight = false;
+                _spriteRenderer.flipX = true;
+            }
+        }
+        else
+            _direction = MoveDirection.None;
+
+	}
+
+    void FixedUpdate() {
+
+        var factor = (int)_direction * Time.fixedDeltaTime * unitsPerSecond;
+        _rb2d.position += Vector2.right * factor;
+
+    }
+
+    
+}
