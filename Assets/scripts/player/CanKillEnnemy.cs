650050bf7dc689d61b9cefd0bb2d20c8f158281a
Aphax <aphax91@gmail.com>
2017-07-30 19:02:04 +0200

    Meilleur hitbox des sauts sur les ennemis

--- Assets/scripts/player/CanKillEnnemy.cs
+++ Assets/scripts/player/CanKillEnnemy.cs
@@ -1,43 +1,49 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class CanKillEnnemy : MonoBehaviour
 {
-	public float bounceForce = 5f;
-	
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
-	
-	void OnCollisionEnter2D(Collision2D collision)
-	{
-		// Contact avec un ennemi
-		if (collision.gameObject.layer == LayerMask.NameToLayer("Ennemies"))
-		{
-//			Debug.Log("CanKillEnnemy.OnCollisionEnter2D");
-			RaycastHit2D[] hitsBottom = Physics2D.RaycastAll(transform.position, Vector2.down);
+    public float bounceForce = 5f;
 
-			foreach (RaycastHit2D raycastHit in hitsBottom)
-			{
-				// Un ennemi est touché par le bas
-				if (raycastHit.rigidbody != null && raycastHit.rigidbody.gameObject.layer == LayerMask.NameToLayer("Ennemies"))
-				{
-					ZombileBehaviour zombileBehaviour = raycastHit.rigidbody.gameObject.GetComponent<ZombileBehaviour>();
-					if (zombileBehaviour != null)
-					{
-						zombileBehaviour.Kill();
-						// Faire rebondir le joueur
-						GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounceForce, ForceMode2D.Impulse);					
-					}
-				}
-			}
-		}
-	}
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        // Contact avec un ennemi
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ennemies"))
+        {
+//			Debug.Log("CanKillEnnemy.OnCollisionEnter2D");
+            List<RaycastHit2D> hitsBottom = new List<RaycastHit2D>(Physics2D.RaycastAll(transform.position, Vector2.down));
+            hitsBottom.AddRange(Physics2D.RaycastAll(
+                new Vector3(transform.position.x, transform.position.y - 16, transform.position.z), Vector2.down));
+            hitsBottom.AddRange(Physics2D.RaycastAll(
+                new Vector3(transform.position.x, transform.position.y + 16, transform.position.z), Vector2.down));
+
+            foreach (RaycastHit2D raycastHit in hitsBottom)
+            {
+                // Un ennemi est touché par le bas
+                if (raycastHit.rigidbody != null &&
+                    raycastHit.rigidbody.gameObject.layer == LayerMask.NameToLayer("Ennemies"))
+                {
+                    ZombileBehaviour zombileBehaviour = raycastHit.rigidbody.gameObject.GetComponent<ZombileBehaviour>();
+                    if (zombileBehaviour != null)
+                    {
+                        zombileBehaviour.Kill();
+                        // Faire rebondir le joueur
+                        GetComponent<Rigidbody2D>().AddForce(Vector2.up * bounceForce, ForceMode2D.Impulse);
+                    }
+                }
+            }
+        }
+    }
 }
