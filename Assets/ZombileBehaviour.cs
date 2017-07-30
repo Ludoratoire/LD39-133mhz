9f8b0d4ae244a71b80e6484b9eed2a75814a488e
Aphax <aphax91@gmail.com>
2017-07-30 18:24:29 +0200

    Mode charge des zombies

--- Assets/ZombileBehaviour.cs
+++ Assets/ZombileBehaviour.cs
@@ -8,6 +8,11 @@ public class ZombileBehaviour : MonoBehaviour
 {
     private Transform _transform;
     private EnnemyWalk _ennemyWalk;
+    private Animator _animator;
+    private Boolean _fighting = false;
+    public int seePlayerY;
+    public int fightVelocity;
+    public int visionRange;
 
     public enum EnnemyType
     {
@@ -23,6 +28,7 @@ public class ZombileBehaviour : MonoBehaviour
         _transform = GetComponent<Transform>();
         _player = GameObject.FindGameObjectWithTag("Player");
         _ennemyWalk = GetComponent<EnnemyWalk>();
+        _animator = GetComponent<Animator>();
     }
 
     void FixedUpdate()
@@ -42,34 +48,53 @@ public class ZombileBehaviour : MonoBehaviour
     // Et si le joueur est à une distance inférieure à 300px (champ de vision)
     private void FollowPlayer()
     {
-        Boolean samePlatform = Mathf.Abs(_player.transform.position.y - _transform.position.y) < 10;
-        if (!samePlatform)
+        Boolean samePlatform = Mathf.Abs(_player.transform.position.y - _transform.position.y) < seePlayerY;
+        if (!samePlatform) 
         {
             return;
         }
         
         if (PlayerInSight(Vector2.left))
         {
+            _animator.SetTrigger("zombileFight");
             _ennemyWalk.SetDirection(EnnemyWalk.MoveDirection.Left);
+            _ennemyWalk.unitsPerSecond = fightVelocity;
+            _fighting = true;
+            return;
         }
+        
         if (PlayerInSight(Vector2.right))
         {
+            _animator.SetTrigger("zombileFight");
             _ennemyWalk.SetDirection(EnnemyWalk.MoveDirection.Right);
+            _ennemyWalk.unitsPerSecond = fightVelocity;
+            _fighting = true;
+            return;
         }
+        
+        _fighting = false;
+        _ennemyWalk.unitsPerSecond = 1;
+        _animator.SetTrigger("zombileWalk");
     }
 
     Boolean PlayerInSight(Vector2 direction)
     {
-        RaycastHit2D[] raycast = Physics2D.RaycastAll(_transform.position, direction, 300);
+        RaycastHit2D[] raycast = Physics2D.RaycastAll(_transform.position, direction, visionRange);
 
         foreach (RaycastHit2D hit in raycast)
         {
-            if (hit.rigidbody == null || hit.rigidbody.gameObject.layer == LayerMask.NameToLayer("Plateforms"))
+            if (hit.collider == null)
+            {
+                continue;
+            }
+            
+           // les zombies ne voient pas a travers les plateformes
+            if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Plateforms"))
             {
                 return false;
             }
 
-            if (hit.rigidbody.gameObject.layer == LayerMask.NameToLayer("Player"))
+            if (hit.collider.gameObject.layer == LayerMask.NameToLayer("Player"))
             {
                 return true;
             }
@@ -89,8 +114,13 @@ public class ZombileBehaviour : MonoBehaviour
         }
     }
 
+    // Detection des murs invisbles pour les ennemis
     void OnTriggerEnter2D(Collider2D collider2D)
     {
+        if (_fighting) {
+            return;
+        }
+        
         if (collider2D.gameObject.layer == LayerMask.NameToLayer("EnnemyWalls"))
         {
 //			Debug.Log("ZombileBehaviour.OnTriggerEnter2D : " + collider2D.gameObject.name);
