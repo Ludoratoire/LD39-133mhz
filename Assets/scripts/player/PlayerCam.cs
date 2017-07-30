31576f84ad0e2610512e529d07df7abe491b1ae7
OuichesDoctor <junkerlucas@gmail.com>
2017-07-30 09:55:50 +0200

    Main Scene : fixes

---
+++ Assets/scripts/player/PlayerCam.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCam : MonoBehaviour {
+
+    public GameObject target;
+
+    private Vector3 _delta;
+
+	void Start () {
+        _delta = gameObject.transform.position - target.transform.position;
+	}
+
+    void LateUpdate () {
+        gameObject.transform.position = new Vector3(target.transform.position.x, gameObject.transform.position.y, gameObject.transform.position.z);
+	}
+}
