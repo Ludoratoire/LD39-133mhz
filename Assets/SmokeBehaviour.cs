04eb451114c5d96c04e615d34ad0f8eeba4d9559
OuichesDoctor <junkerlucas@gmail.com>
2017-07-31 11:26:03 +0200

    Pit & Smoke Behaviour

--- Assets/SmokeBehaviour.cs
+++ Assets/SmokeBehaviour.cs
@@ -2,19 +2,16 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PitBehaviour : MonoBehaviour {
+public class SmokeBehaviour : MonoBehaviour {
 
     void OnTriggerEnter2D(Collider2D collision) {
 
-        if(collision.gameObject.tag == "Player" && collision.GetType() == typeof(CapsuleCollider2D)) {
+        if (collision.gameObject.tag == "Player" && collision.GetType() == typeof(CapsuleCollider2D)) {
             var mgr = GameManager.Instance;
             mgr.life--;
             if (mgr.life <= 0) {
                 mgr.score = 0;
                 mgr.ResetPlayerPos();
-            }
-            foreach (var c in collision.gameObject.GetComponents<Collider2D>()) {
-                c.enabled = false;
             }
         }
 
