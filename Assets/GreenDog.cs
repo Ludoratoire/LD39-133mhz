3099be2d15777708655090b01a4a7e8ba8ba1f18
Nujerk <junkerlucas@gmail.com>
2017-07-31 13:32:16 +0200

    Green Dog

---
+++ Assets/GreenDog.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GreenDog : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D collision) {
+        if(collision.gameObject == GameManager.Instance.player && collision is CapsuleCollider2D) {
+            GameManager.Instance.Victory();
+        }
+    }
+
+}
