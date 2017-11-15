3e444dc5ee6117125594985db8774ad7ec20c1ac
OuicheDoctor <junkerlucas@gmail.com>
2017-11-15 17:39:25 +0100

    Create level editor scripts to create, load & save new scenes as level
    Signed-off-by: OuicheDoctor <junkerlucas@gmail.com>

---
+++ Assets/editor/level_editor/LevelLoaderEditor.cs
@@ -0,0 +1,31 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(LevelLoader))]
+public class LevelLoaderEditor : Editor {
+
+    public override void OnInspectorGUI() {
+
+        LevelLoader component = (LevelLoader)target;
+        component.selectedLevelField = EditorGUILayout.ObjectField("Selected Scene", component.selectedLevelField, typeof(SceneAsset), false);
+
+
+        if (GUILayout.Button("New level")) {
+            component.CreateLevel();
+        }
+
+        if (GUILayout.Button("Open level")) {
+            component.LoadLevel();
+        }
+
+        if (GUILayout.Button("Save level")) {
+            component.SaveLevel();
+        }
+
+        if (GUILayout.Button("Close level")) {
+            component.UnloadCurrentLevel();
+        }
+
+    }
+
+}
---
+++ Assets/scripts/level_editor/LevelLoader.cs
@@ -0,0 +1,47 @@
+﻿using UnityEditor;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+[ExecuteInEditMode]
+public class LevelLoader : MonoBehaviour {
+
+    public Object selectedLevelField;
+
+    private Scene _currentlyLoadedLevel;
+
+    public void CreateLevel() {
+        // First remove current level if any
+        UnloadCurrentLevel();
+
+        _currentlyLoadedLevel = EditorSceneManager.NewScene(NewSceneSetup.EmptyScene, NewSceneMode.Additive);
+    }
+
+    public void LoadLevel() {
+        // First remove current level if any
+        UnloadCurrentLevel();
+
+        // Then add the content of the selected level to the master scene
+        _currentlyLoadedLevel = EditorSceneManager.OpenScene(AssetDatabase.GetAssetPath(selectedLevelField), OpenSceneMode.Additive);
+    }
+
+    public void SaveLevel() {
+        if (_currentlyLoadedLevel.path != null && _currentlyLoadedLevel.isDirty)
+            EditorSceneManager.SaveScene(_currentlyLoadedLevel);
+    }
+
+    public void UnloadCurrentLevel() {
+        if (_currentlyLoadedLevel.path != null) {
+            // Ask if we should save the modif on the level to unload
+            if (_currentlyLoadedLevel.isDirty && EditorSceneManager.SaveModifiedScenesIfUserWantsTo(new Scene[] { _currentlyLoadedLevel })) {
+                EditorSceneManager.SaveScene(_currentlyLoadedLevel);
+            }
+
+            // Unload the level
+            EditorSceneManager.CloseScene(_currentlyLoadedLevel, true);
+        }
+    }
+
+
+}
+
